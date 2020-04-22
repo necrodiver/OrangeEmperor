84355a9ad41c985565b46e03f014bf1d885ddbdf
neclodiver <neclodiver@live.com>
2020-04-22 18:15:58 +0800

    添加sentry错误查询插件，因为是开放平台就没有单独拉个服务器，使用官方提供的

--- oe-ob/src/index.js
+++ oe-ob/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/browser';
 import App from './Components/App/App';
 import { BrowserRouter,HashRouter } from 'react-router-dom';
 import './index.less';
+
+Sentry.init({dsn: "https://99cb561cc36a45d6940aab6961b3489f@o381675.ingest.sentry.io/5209347"});
 
 ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
